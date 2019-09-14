f1a371b5242dd31945ea19f22d6bc21b811dfb1c
Bidisha Mondal <mondal.bidisha@talview.com>
2019-09-15 00:11:01 +0530

    login page init

--- orange/src/App.js
+++ orange/src/App.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// export default App;
+export default App;
--- orange/src/index.js
+++ orange/src/index.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
+import Signin from './Signin'
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Hello, React!</h1>
-			</div>
+			<Signin />
 		)
 	}
 }
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+
